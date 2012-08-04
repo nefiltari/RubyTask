10dd4675d5c9d1e5c0ee082239bdafafaccbee77
Francesco Möller <splashfire@web.de>
2012-08-04 20:04:50 +0200

    Spira Test

--- app/models/organisation.rb
+++ app/models/organisation.rb
@@ -2,18 +2,18 @@ require 'rdf'
 require 'rdf/ntriples'
 require 'rdf/sesame'
 require 'spira'
-include RDF
 
-Spira.add_repository! :ruta, Sesame::Repository.new("http://localhost:8080/openrdf-sesame/repositories/ruta")
+
+Spira.add_repository! :default, RDF::Sesame::Repository.new("http://localhost:8080/openrdf-sesame/repositories/ruta")
 
 class Organisation
 
   include Spira::Resource
 
-  base_uri URI.new("http://rubytask.org/")
-  default_vocabulary URI.new("http://rubytask.org/")
+  base_uri RDF::URI.new("http://rubytask.org/")
+  default_vocabulary RDF::URI.new("http://rubytask.org/")
 
-  property :has_name, type: XSD.String
-  property :has_description, type: XSD.String
+  property :has_name, type: RDF::XSD.string
+  property :has_description, type: RDF::XSD.string
 
 end
