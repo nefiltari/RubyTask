28ec8f9a3ff122f89131fe0d230f46491199f826
Ramastuh <ramastuh@Nefiltop.(none)>
2012-08-04 18:01:30 +0200

    Initial Release

---
+++ config/initializers/secret_token.rb
@@ -0,0 +1,7 @@
+# Be sure to restart your server when you modify this file.
+
+# Your secret key for verifying the integrity of signed cookies.
+# If you change this key, all old signed cookies will become invalid!
+# Make sure the secret is at least 30 characters and all random,
+# no regular words or you'll be exposed to dictionary attacks.
+RubyTask::Application.config.secret_token = '69eb1cadac8e6976fd3f194ca7f28454be29515ae75d7b873e7d5b066e04dbc23b4c06196e8d3834139e61f93bb1d959764ff85cedcc192a366eeb89cd851c6c'
